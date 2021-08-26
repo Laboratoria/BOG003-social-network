0fe8c4357c2a61e4ad93c2fd0033619513b51fe0
Maria-Camila123 <na01alvarez@gmail.com>
2021-08-26 13:43:40 -0500

    template register

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 // Este es el punto de entrada de tu aplicacion
-
+import { templateRegister } from './lib/view/template-register.js'
 import { myFunction } from './lib/index.js'
 
-myFunction()
+myFunction();
+
+document.getElementById("s-container").innerHTML = templateRegister();
