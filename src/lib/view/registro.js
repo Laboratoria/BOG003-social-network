02f0c8026566f9682ff78e3dc413b528dd5e2459
Karen Poveda Lasprilla <karenpoveda29@gmail.com>
2021-08-27 23:50:34 -0500

    Agregar el html de la vista registro

--- src/lib/view/registro.js
+++ src/lib/view/registro.js
@@ -1,8 +1,30 @@
-export const irAInicio = () => {
-    const linkCuenta = `
-    <div>
+export const createRegisterView = () => {
+  const registerSection = document.createElement("section");
+  const registerView = `
+  <div class="brand">
+    <div class="logo-long"><img src="../../images.logo_long" alt="Behind Code"></div>
+    <div class="señor"><img src="../../images/señor.png" alt="Logo for desktop"></div>
+  </div>
+  <div class="register">
+    <h1>Formulario de Registro</h1>
+    <form method="post">
+      <input type="text" name="name" placeholder="Nombre" required>
+      <input type="text" name="lastName" placeholder="Apellido" required>
+      <input type="email" name="email" placeholder="Correo" required>
+      <input type="password" name="password" placeholder="Contraseña" required>
+      <button type="submit">Regístrate</button>
+    </form>
+    <div class="existing-account">
     <a href="#/ingreso">¿Ya tienes una cuenta?</a>
     </div>
-    `
-    return linkCuenta;
+    <p>or</p>
+    <p>Ingresa con </p>
+    <div class="Google-login">
+      <a href="/">
+        <img src="../../images/google.png" alt="Log in with Google">
+      </a>
+    </div>    
+  </div>`;
+  registerSection.innerHTML = registerView;  
+  return registerSection;
 }
