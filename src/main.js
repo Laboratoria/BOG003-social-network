84e528ad930b887ffce4d58aa673406c43c05bf4
Karen Poveda Lasprilla <karenpoveda29@gmail.com>
2021-08-27 23:46:24 -0500

    Modificar función init

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from "./lib/index.js";
-import { crearCuenta } from "./lib/view/ingreso.js";
-import { irAInicio } from "./lib/view/registro.js";
+/* import { myFunction } from "./lib/index.js"; */
+import { createLogInView } from "./lib/view/ingreso.js";
 import { changeRoute } from "./lib/router.js";
 
 
 const init = () => {
-  document.getElementById("root").innerHTML = crearCuenta();
-  window.addEventListener("hashchange", () => {
-    myFunction();
-    console.log(window.location.hash);
-    changeRoute(window.location.hash)
+  document.getElementById('main-section').appendChild(createLogInView());
+  window.addEventListener('hashchange', () => {
+    /* myFunction(); */
+    changeRoute(window.location.hash);
   })
 }
 
-window.addEventListener("load", init);
+window.addEventListener('load', init);
