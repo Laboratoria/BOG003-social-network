4e1b178a1337222c49e989593d21704b3ae6e488
Karen Poveda Lasprilla <karenpoveda29@gmail.com>
2021-08-31 11:27:59 -0500

    registro de usuario

--- src/lib/view/registro.js
+++ src/lib/view/registro.js
@@ -1,5 +1,8 @@
 import { createUser } from '../api/firebaseConnection.js';
 
+let email;
+let password;
+
 export const createRegisterView = () => {
   const registerSection = document.createElement("section");
   const registerView = `
@@ -14,10 +17,10 @@ export const createRegisterView = () => {
     <div class="log-in">
       <h1>Formulario de Registro</h1>
       <form method="post">
-        <input type="text" name="name" placeholder="Nombre" required>
-        <input type="text" name="lastName" placeholder="Apellido" required>
-        <input type="email" name="email" placeholder="Correo" required>
-        <input type="password" name="password" placeholder="Contraseña" required>
+        <input id="userName" type="text" name="name" placeholder="Nombre" required>
+        <input id="lastName" type="text" name="lastName" placeholder="Apellido" required>
+        <input id="email" type="email" name="email" placeholder="Correo" required>
+        <input id="password" type="password" name="password" placeholder="Contraseña" required>
         <button id="register" type="submit">Regístrate</button>
       </form>
       <div class="new-account">
@@ -34,13 +37,24 @@ export const createRegisterView = () => {
   registerSection.innerHTML = registerView;
   setTimeout(() => {
     addListeners();
-  }, 0);
+  }, 1000);
   return registerSection;
 }; 
 
+/* const userName = document.getElementById('userName').value;
+const lastName = document.getElementById('lastName').value; */
+
+
+
 const addListeners = () => {
   const btnRegister = document.getElementById('register');
-  btnRegister.addEventListener('click', () => createUser('karen@gmail.com', '123456*Ab'))
+  btnRegister.addEventListener('click', (e) => {
+    email = document.getElementById('email').value;
+    password = document.getElementById('password').value;
+    createUser(email, password);
+  })
 }
 
 //obtener la info del usuario
+
+/* npm install firebase-mock --save-dev */
