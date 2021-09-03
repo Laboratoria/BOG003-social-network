630bb636d83e035445bce693052a41ae145a7e34
Giselle <kgiselle17@gmail.com>
2021-09-03 11:52:15 -0500

    cambio stilos CSS para adaptar a Desktop con @mediaQuery

---
+++ src/lib/templates/login.js
@@ -0,0 +1,48 @@
+export const login = () => {
+    //Div contenedor de la vista div padre
+    const divLogin = document.createElement('div');
+    divLogin.setAttribute("class", "divFlex");
+
+    //creando tag h1 titulo
+    const viewLogin = `
+        <div class = "welcomeContainer">
+            <h1>OtaKuGeeK <br>オタク</h1>
+            <img src="IMG/sombra-logo.svg" width="100" alt="logo sin fondo" class="logo">
+            <h2>¡Bienvendos!</h2>
+            <h3>Red social para Otakus y no tan Otakus</h3>
+        </div>
+    
+        <div class="divForm">
+            <div class="formContainer">
+                <h2>Log In</h2>
+                <h3>Es fácil, rápido y divertido!!!</h3>
+                    <form id="signUp">
+                        <input type="email" id="email" class="signUp" placeholder="Ingresa tu Email" required="" autocomplete="off"></input>
+                    </form>
+
+                    <form id="signUp1">
+                        <input type="password" id="password" class="signUp" placeholder="Ingresa tu Contraseña" pattern=".{8,}" minlength="8" maxlength="32" required="" autocomplete="off"></input>
+                    </form>
+
+                    <button type="submit" id="btnregister" class="btnregister" onclick="">Registrarte</button>
+
+                <h4>O registrate con...</h4>
+
+                <a href=""><img src="IMG/Icono-google.svg" width="40" alt="icono" class="iconGoogle"></a>
+            </div>
+
+            <div class="logContainer">
+                <h4>¿Ya tienes una cuenta?</h4>
+                <a href="#/login" class="linkLogin">Entrar</a>
+            </div>
+        </div>
+        `
+        
+    //Mostrar en pantalla tag h1    
+    divLogin.innerHTML = viewLogin;
+
+    
+
+    return divLogin;
+    
+}
