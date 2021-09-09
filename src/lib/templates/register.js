4dd78607e4285273749799baaf1649f9c456e9cb
laurapalma <lau.palma9617@gmail.com>
2021-09-09 17:19:44 -0500

    Correcció de errores eslint y funcionalidad HU1

--- src/lib/templates/register.js
+++ src/lib/templates/register.js
@@ -1,68 +1,119 @@
-/* import { login } from "./login"; */
-//Importar función regGoogle y regEmail fromm auth.js
-import { regGoogle, regEmail } from '../firebase/auth.js';
-
-
+/* import { login } from './login'; */
+// Importar función regGoogle y regEmail fromm auth.js
+import { regGoogle, createUser, emailVerification } from '../firebase/auth.js';
 
 export const register = () => {
-    //Div contenedor de la vista div padre
-    const divRegister = document.createElement('div');
-    divRegister.setAttribute("class", "divFlex");
+  //  Div contenedor de la vista div padre
+  const divRegister = document.createElement('div');
+  divRegister.setAttribute('class', 'divFlex');
 
-    //creando esquema HTML
-    const viewRegister = `
-        <div class = "welcomeContainer">
+  // creando esquema HTML
+  const viewRegister = `
+        <div class = 'welcomeContainer'>
             <h1>OtaKuGeeK <br>オタク</h1>
-            <img src="IMG/sombra-logo.svg" width="100" alt="logo sin fondo" class="logo">
+            <img src='IMG/sombra-logo.svg' width='100' alt='logo sin fondo' class='logo'>
             <h2>¡Bienvendos!</h2>
             <h3>Red social para Otakus y no tan Otakus</h3>
         </div>
     
-        <div class="divForm">
-            <div class="formContainer">
+        <div class='divForm'>
+            <div class='formContainer'>
                 <h2>Crea una cuenta</h2>
                 <h3>Es fácil, rápido y divertido!!!</h3>
                     
-                <input type="email" id="email" class="signUp" placeholder="Ingresa tu Email" required="" autocomplete="off"></input>
+                <input type='email' id='email' class='signUp' placeholder='Ingresa tu Email' required='' autocomplete='off'></input>
                 
-                <input type="password" id="password" class="signUp" placeholder="Ingresa tu Contraseña" pattern=".{8,}" minlength="8" maxlength="32" required="" autocomplete="off"></input>
+                <input type='password' id='password' class='signUp' placeholder='Ingresa tu Contraseña' pattern='.{8,}' minlength='8' maxlength='32' required='' autocomplete='off'></input>
             
-                <button type="button" id="btnregister" class="btnregister">Registrarte</button>
+                <input type='password' id='confirmPassword' class='signUp' placeholder='Confirma tu Contraseña' pattern='.{8,}' minlength='8' maxlength='32' required='' autocomplete='off'></input>
+
+                <button type='button' id='btnregister' class='btnregister'>Registrarte</button>
+
+                <span id='errorMessage' class='errorMessage'></span>
 
                 <h4>O registrate con...</h4>
 
-                <a href=""><img src="IMG/Icono-google.svg" width="40" alt="icono" class="iconGoogle"></a>
+                <a href=''><img src='IMG/Icono-google.svg' width='40' alt='icono' class='iconGoogle'></a>
             </div>
 
-            <div class="logContainer">
+            <div class='logContainer'>
                 <h4>¿Ya tienes una cuenta?</h4>
-                <a href="#/login" class="linkLogin">Inicia Sesión</a>
+                <a href='#/login' class='linkLogin'>Inicia Sesión</a>
             </div>
         </div>
-        `
-        
-        //Mostrar en pantalla tag h1    
-        divRegister.innerHTML = viewRegister;
+        `;
 
-        /* Aquí se le agrega el evento click al botón register, se capturan los valores de los inputs
+  //  Mostrar en pantalla tag h1
+  divRegister.innerHTML = viewRegister;
+
+  /* Aquí se le agrega el evento click al botón register, se capturan los valores de los inputs
         y se hacen las validaciones respectivas (capturar errores) */
-        const btnRegister = divRegister.querySelector('#btnregister');
-        btnRegister.addEventListener('click', () => {
-            const email = divRegister.querySelector('#email').value;
-            const password = divRegister.querySelector('#password').value;
-            console.log("email="+email+" password="+password);
-            regEmail(email, password);
+  const btnRegister = divRegister.querySelector('#btnregister');
+  btnRegister.addEventListener('click', () => {
+    const email = divRegister.querySelector('#email').value;
+    const password = divRegister.querySelector('#password').value;
+    const confirmPassword = divRegister.querySelector('#confirmPassword').value;
+    const errorMessage = divRegister.querySelector('#errorMessage');
+    console.log('email=', email, ' password=', password, confirmPassword);
+
+    if ((password === confirmPassword) !== '') {
+      //  Traer la variable createUser para registrase en la app con usuario y contraseña
+      createUser(email, password)
+        .then(() => {
+          const user = firebase.auth().currentUser;
+          user.updateProfile({
+            displayName: email,
+          });
+          return emailVerification();
+        })
+        .then(() => {
+          divRegister.querySelector('#errorMessage').innerHTML = 'Usuario Creado, verifica tu correo electronico para verificar la cuenta';
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          switch (errorCode) {
+            case 'auth/invalid-email':
+              errorMessage.innerHTML = 'Por favor ingrese un correo valido';
+              break;
+            case 'auth/email-already-in-use':
+              errorMessage.innerHTML = 'Ya existe un usuario con este correo';
+              break;
+            case 'auth/weak-password':
+              errorMessage.innerHTML = 'La contraseña debe tener minimo seis digitos';
+              break;
+            default:
+              errorMessage.innerHTML = 'Ha ocurrido un error inesperado';
+              break;
+          }
         });
+    } else {
+      errorMessage.innerHTML = 'Las contraseñas no coinciden';
+    }
+  });
 
-        //Funcionalidad registro con GOOGLE
-        const btnGoogleRegister = divRegister.querySelector('.iconGoogle');
-        btnGoogleRegister.addEventListener("click", (e) => {
-            e.preventDefault()
-            regGoogle();
-        });
+  /*
+  xport const regEmail = () => {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
+}; */
 
+  // Funcionalidad registro con GOOGLE
+  const btnGoogleRegister = divRegister.querySelector('.iconGoogle');
+  btnGoogleRegister.addEventListener('click', (e) => {
+    e.preventDefault();
+    regGoogle();
+  });
 
-    return divRegister;
-    
-}
-
+  return divRegister;
+};
