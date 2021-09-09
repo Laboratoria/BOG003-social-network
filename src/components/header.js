aeac25c905c6238b5c314a0e4b86aacb0db02a03
natafajardo <79486039+natafajardo@users.noreply.github.com>
2021-09-09 14:44:40 -0500

    configuracion header.js

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+export const header = `
+<nav>
+  <ul>
+    <li>
+      <a href="#/">Home</a>
+    </li>
+    <li>
+      <a href="#/posts">Posts</a>
+    </li>
+    <li>
+      <a href="#/login">Login</a>
+    </li>
+    <li>
+      <a href="#/register">Register</a>
+    </li>
+  </ul>
+</nav>
+
+`;
--- src/views/posts.js
+++ src/views/posts.js
@@ -1,3 +1,6 @@
+import { header } from '../components/header.js';
+
 export const postsView = `
+ ${header}
     <h1>from posts</h1>
 `;
