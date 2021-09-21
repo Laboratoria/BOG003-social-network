ca6179a7b0b8a4e191ebcae004ddf49982a45d6d
Xiomara <xiomarach2000@gmail.com>
2021-09-21 14:20:11 -0500

    Maquetación de la vista del post.

---
+++ src/views/post.js
@@ -0,0 +1,32 @@
+export const postView = `
+<header class="headerPets">
+    <div class="menuA">
+      <div class="logoA">
+        <a class="logo-A"><img src="img/logotipo.png" alt="Logotipo"></a>
+      </div>
+    </div>
+  </header>
+  <div>
+    <nav class="nav">
+      <a href="#"><img src="img/home-page.png" alt="Home"></a>
+      <a href="#"><img src="img/paw.png" alt="Perfil"></a>
+      <a href="#"><img src="img/alarm.png" alt="Notificaciones"></a>
+    </nav>
+  </div>
+  <section class="postSection">
+    <div class="prePost">
+      <a class="hands"><img src="img/hand-animals-human.png" alt="Hand-animals-human"/></a>
+      <h1>Crea tu publicación</h1>
+      <p>¡¡No olvides los mejores momentos al lado de tu humano!!</p>
+    </div>
+    <div class="post">
+      <a class="imgProfile"><img src="img/cat.png" alt="profile"/></a>
+      <a class="imgSecurity"><img src="img/security.png" alt="Security"/></a>
+      <a class="imgMenuVertical"><img src="img/menu-vertical.png" alt="Menu Vertical"/></a>
+    </div>
+    <input type="text" placeholder="¿Que quieres publicar hoy?">
+  </section>
+  <footer class="footerA">
+    <div class="sections">
+  </footer>
+  `;
