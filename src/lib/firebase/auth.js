5151f2ba06e601efe7c4ff4df33d35a0755088d9
laurapalma <lau.palma9617@gmail.com>
2021-09-30 16:28:50 -0500

    Funcionalidad al boton Like

--- src/lib/firebase/auth.js
+++ src/lib/firebase/auth.js
@@ -21,25 +21,23 @@ export const loginGoogle = () => {
     .auth()
     .signInWithPopup(provider)
     .then((result) => {
-      /** @type {firebase.auth.OAuthCredential} */
-      // const credential = result.credential;
-
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      // const token = credential.accessToken;
-      // The signed-in user info.
       const user = result.user;
       console.log('user', user);
-      // ...
     })
     .catch((error) => {
-      // Handle Errors here.
-      // const errorCode = error.code;
       const errorMessage = error.message;
-      // The email of the user's account used.
-      // const email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      // const credential = error.credential;
       console.log('error', errorMessage);
       // ...
     });
 };
+
+// Funcion para dar like en firestore
+const db = firebase.firestore();
+export const addLike = (userId, id) => db.collection('postList').doc(id).update(({
+  likes: firebase.firestore.FieldValue.arrayUnion(userId),
+}));
+
+export const removeLike = (userId, id) => db.collection('postList').doc(id).update(({
+  likes: firebase.firestore.FieldValue.arrayRemove(userId),
+}));
+
